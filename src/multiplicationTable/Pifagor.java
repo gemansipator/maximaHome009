4b5bd1e2b44f7bba8952f1ced904da7b41b16ce4
gemansipator <gemansipator@gmail.com>
2023-01-12 14:50:18 +0400

    Таблица умножения

---
+++ src/multiplicationTable/Pifagor.java
@@ -0,0 +1,20 @@
+package multiplicationTable;
+
+public class Pifagor {
+    public static void main(String[] args) {
+
+        int a;
+        int b;
+        for (int i = 1; i < 10; i++) {
+            System.out.print(" ");
+            for (int j = 1; j < 10; j++) {
+                System.out.print(i * j + "   ");
+            }
+            System.out.println();
+
+            
+        }
+
+
+    }
+}

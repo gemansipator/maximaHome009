18f433aff487d2f7ce469528222f8b7495fd17bc
gemansipator <gemansipator@gmail.com>
2023-01-15 19:55:46 +0400

    Задача 6 (Рекурсия)
    Решите при помощи рекурсии и цикла ( 2 разных решения) задачу перевода из десятичной системы счисления в двоичную.

--- src/integer/to/binary/IntegerBinaryLoop.java
+++ src/integer/to/binary/IntegerBinaryLoop.java
@@ -1,4 +1,22 @@
 package integer.to.binary;
 
+import java.util.Scanner;
+
 public class IntegerBinaryLoop {
+    public static void binar(int a) {
+        int i, b;
+        while(a !=0 ) {
+            b = a%2;
+            System.out.print(b);
+            a = a/2;
+        }
+    }
+    public static void main(String [] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Введите целое число: ");
+        int binar = s.nextInt();
+        binar(binar);
+
+
+    }
 }
--- src/integer/to/binary/IntegerBinaryRecursion.java
+++ src/integer/to/binary/IntegerBinaryRecursion.java
@@ -1,5 +1,24 @@
 package integer.to.binary;
 
+import java.util.Scanner;
+
 public class IntegerBinaryRecursion {
+    public static String toBinary(int n)
+    {
+        if (n == 0) {
+            return "";
+        }
+        return toBinary(n / 2) + (n % 2);
+    }
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Введите целое число: ");
+        int n = s.nextInt();
+        int length = 12;
+
+        String binary = String.format("%0" + length + "d", Integer.valueOf(toBinary(n)));
+        System.out.println(binary);
+    }
 
 }
